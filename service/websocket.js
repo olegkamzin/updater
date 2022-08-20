dcf0096ef097a9542dc36f8f42845f91e7b51143
Kamzin <olegkamzin@gmail.com>
2022-08-20 19:17:30 +0300

    new

--- service/websocket.js
+++ service/websocket.js
@@ -1,7 +1,7 @@
 
 import { WebSocketServer } from 'ws'
 
-const wss = new WebSocketServer({ port: 8080 })
+const wss = new WebSocketServer({ port: 3001 })
 
 wss.on('connection', ws => {
 	ws.send('CONNECTED')
